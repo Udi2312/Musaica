3857891ca7d0b978779f811a5e10a0ac8c123bd8
yogimait <145682078+yogimait@users.noreply.github.com>
2025-03-16 21:33:44 +0530

    Update Project.js

--- Musaica/server/models/Project.js
+++ Musaica/server/models/Project.js
@@ -4,6 +4,10 @@ const ProjectSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Link to user
   filename: { type: String, required: true },
   username: { type: String, required: true }, // Add this line
+  processedFiles: [{
+    type: String, // Path to processed file (e.g., "processed/trimmed-song.mp3")
+    effect: String // "trim", "speed", etc.
+  }],
   effects: { type: [String], default: [] }, // e.g., ["reverb", "delay"]
   createdAt: { type: Date, default: Date.now },
 });
